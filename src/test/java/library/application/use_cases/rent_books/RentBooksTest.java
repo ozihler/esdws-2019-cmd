90bf261685b5bfd0f76ad1e3d5c2c4e437a949a5
ozihler <ozihler@gmail.com>
2019-10-06 17:00:43 +0200

    Finish testing

--- src/test/java/library/application/use_cases/rent_books/RentBooksTest.java
+++ src/test/java/library/application/use_cases/rent_books/RentBooksTest.java
@@ -1,13 +1,14 @@
 package library.application.use_cases.rent_books;
 
 import library.application.outbound_ports.BookRepository;
-import library.application.outbound_ports.CustomerRepository;
 import library.application.use_cases.rent_books.ports.RentBookRequest;
 import library.application.use_cases.rent_books.ports.RentBooksInput;
 import library.application.use_cases.rent_books.ports.RentBooksRequest;
 import library.domain.entities.Book;
 import library.domain.entities.Customer;
+import library.domain.values.Rental;
 import library.domain.values.RentalDocument;
+import library.domain.values.RentalRecordDocument;
 import org.junit.Test;
 
 import java.util.List;
@@ -16,13 +17,16 @@ import static org.junit.Assert.*;
 
 public class RentBooksTest {
 
+    private Book book;
+    private RentBooksInput rentBooksInput;
+
     @Test
     public void testExecuteWith() {
-        Book book = new Book(
+        book = new Book(
                 1,
                 "Hello",
                 "World",
-                "ReadingMode",
+                "BOTH",
                 "link"
         );
 
@@ -42,15 +46,23 @@ public class RentBooksTest {
 
         RentBookRequest rentBookRequest = new RentBookRequest(1, 5);
         RentBooksRequest rentBooksRequest = new RentBooksRequest("userX", List.of(rentBookRequest));
-        RentBooksInput rentBooksInput = new RentBooksInput(booksRepository, rentBooksRequest);
+        rentBooksInput = new RentBooksInput(booksRepository, rentBooksRequest);
 
-        rentBooks.executeWith(
-                rentBooksInput,
-                rentalRecord -> {
-                    List<RentalDocument> rentals = rentalRecord.getRentals();
-                    assertEquals(1, rentals.size());
-                    assertEquals(book.getTitle(), rentals.get(0).getBookTitle());
-                });
+        rentBooks.executeWith(rentBooksInput, this::assertResults);
 
     }
+
+    private void assertResults(RentalRecordDocument rentalRecord) {
+        List<RentalDocument> rentals = rentalRecord.getRentals();
+        assertEquals(1, rentals.size());
+        assertEquals(book.getTitle(), rentals.get(0).getBookTitle());
+        assertEquals(book.getAuthors(), rentals.get(0).getBookAuthors());
+
+        assertEquals(rentBooksInput.getCustomerName(), rentalRecord.getCustomerName());
+        assertEquals(rentBooksInput.getRentals().get(0).getDaysRented(), rentals.get(0).getDaysRented());
+        assertEquals(rentBooksInput.getRentals().get(0).getAmount(), rentals.get(0).getAmount(), 0.001);
+        assertEquals(rentBooksInput.getRentals().get(0).getFrequentRenterPoints(), rentalRecord.getFrequentRenterPoints(), 0.001);
+        assertEquals(rentBooksInput.getRentals().get(0).getAmount(), rentalRecord.getTotalAmount(), 0.001);
+    }
+
 }
